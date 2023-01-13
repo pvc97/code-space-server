a03e88b7b9dd23e6bb13605ea0dcc27d34ef0e2d
pvc2209 <cuongpv2209@gmail.com>
2023-01-14 01:12:01 +0700

    Add username and password require.

--- constants/strings.js
+++ constants/strings.js
@@ -3,8 +3,12 @@ const INTERNAL_SERVER_ERROR_MESSAGE = 'Internal Server Error';
 const INVALID_TOKEN_MESSAGE = 'Invalid Token';
 const TOKEN_EXPIRED_MESSAGE = 'Token Expired';
 const TOKEN_REQUIRED_MESSAGE = 'Token is Required';
+const REQUIRED_PASSWORD = 'Required Password';
+const REQUIRED_USERNAME = 'Required Username';
 
 module.exports = {
+  REQUIRED_PASSWORD,
+  REQUIRED_USERNAME,
   LOGIN_ERROR_MESSAGE,
   INVALID_TOKEN_MESSAGE,
   TOKEN_EXPIRED_MESSAGE,
