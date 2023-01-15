88343d1d9e8f96996ce2bb38dc5af1e71047228a
pvc2209 <cuongpv2209@gmail.com>
2023-01-15 11:33:59 +0700

    Add cors.

---
+++ middlewares/cors/cors.js
@@ -0,0 +1,13 @@
+const cors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+};
+
+module.exports = {
+  cors,
+};
