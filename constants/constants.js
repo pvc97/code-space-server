31ee0e599c3e854e641ebbf8caf79d43f361cd4b
pvc2209 <cuongpv2209@gmail.com>
2023-01-18 15:01:59 +0700

    Update token duration.

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
-const ACCESS_TOKEN_DURATION = 10; // 2 minutes
-const REFRESH_TOKEN_DURATION = 10; // 5 minutes
+const ACCESS_TOKEN_DURATION = 300; // 5 minutes
+const REFRESH_TOKEN_DURATION = 3600; // 60 minutes
 
 module.exports = {
   ACCESS_TOKEN_DURATION,
