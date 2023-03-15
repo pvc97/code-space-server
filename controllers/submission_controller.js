c62f97239580e8a73ef29a9605be2019c88226d5
pvc2209 <cuongpv2209@gmail.com>
2023-03-15 22:56:49 +0700

    Submit code to judge0.

--- controllers/submission_controller.js
+++ controllers/submission_controller.js
@@ -1,6 +1,4 @@
-const {
-  getJudge0Submissions: getJudge0Submissions,
-} = require('../services/submission_service');
+const { submit: submit } = require('../services/submission_service');
 
 const {
   Problem,
@@ -19,109 +17,109 @@ const translate = require('../utils/translate');
  * 5. Save the submission and submission results to database
  * 6. Return the formatted submission containing submission results
  */
-const createSubmission = async (req, res) => {
-  try {
-    // Authenticate middleware has attached the user to the request object
-    const userId = req.user.id;
-    const sourceCode = req.body.sourceCode;
-    const problemId = req.body.problemId;
+// const createSubmission = async (req, res) => {
+//   try {
+//     // Authenticate middleware has attached the user to the request object
+//     const userId = req.user.id;
+//     const sourceCode = req.body.sourceCode;
+//     const problemId = req.body.problemId;
 
-    if (!sourceCode) {
-      return res
-        .status(400)
-        .send({ error: translate('required_source_code', req.hl) });
-    }
+//     if (!sourceCode) {
+//       return res
+//         .status(400)
+//         .send({ error: translate('required_source_code', req.hl) });
+//     }
 
-    if (!problemId) {
-      return res
-        .status(400)
-        .send({ error: translate('required_problem_id', req.hl) });
-    }
+//     if (!problemId) {
+//       return res
+//         .status(400)
+//         .send({ error: translate('required_problem_id', req.hl) });
+//     }
 
-    const problem = await Problem.findByPk(problemId, {
-      include: {
-        model: TestCase,
-        as: 'testCases',
-        where: { active: true },
-      },
-    });
+//     const problem = await Problem.findByPk(problemId, {
+//       include: {
+//         model: TestCase,
+//         as: 'testCases',
+//         where: { active: true },
+//       },
+//     });
 
-    const testCases = problem.testCases;
-    const inputSubmissions = [];
-    for (testCase of testCases) {
-      const submission = {
-        source_code: sourceCode,
-        language_id: problem.languageId,
-        stdin: testCase.stdin,
-        expected_output: testCase.expectedOutput,
-      };
+//     const testCases = problem.testCases;
+//     const inputSubmissions = [];
+//     for (testCase of testCases) {
+//       const submission = {
+//         source_code: sourceCode,
+//         language_id: problem.languageId,
+//         stdin: testCase.stdin,
+//         expected_output: testCase.expectedOutput,
+//       };
 
-      inputSubmissions.push(submission);
-    }
+//       inputSubmissions.push(submission);
+//     }
 
-    // Submit code to Judge0
-    const judge0Submissions = await getJudge0Submissions(inputSubmissions);
+//     // Submit code to Judge0
+//     const judge0Submissions = await getJudge0Submissions(inputSubmissions);
 
-    let totalPoint = 0;
-    const submissionResults = [];
-    const results = [];
-    for (var i = 0; i < judge0Submissions.length; i++) {
-      const correct = judge0Submissions[i].status.id == 3;
-      if (correct) {
-        totalPoint += problem.pointPerTestCase;
-      }
+//     let totalPoint = 0;
+//     const submissionResults = [];
+//     const results = [];
+//     for (var i = 0; i < judge0Submissions.length; i++) {
+//       const correct = judge0Submissions[i].status.id == 3;
+//       if (correct) {
+//         totalPoint += problem.pointPerTestCase;
+//       }
 
-      let output =
-        judge0Submissions[i].stdout ||
-        judge0Submissions[i].stderr ||
-        judge0Submissions[i].compile_output ||
-        '';
-      // Replace all \n with ''
-      output = output.replace(/\n/g, '');
-      // Decode base64
-      output = Buffer.from(output, 'base64').toString('ascii');
+//       let output =
+//         judge0Submissions[i].stdout ||
+//         judge0Submissions[i].stderr ||
+//         judge0Submissions[i].compile_output ||
+//         '';
+//       // Replace all \n with ''
+//       output = output.replace(/\n/g, '');
+//       // Decode base64
+//       output = Buffer.from(output, 'base64').toString('ascii');
 
-      const submissionResult = {
-        testCaseId: testCases[i].id,
-        output,
-        correct,
-      };
-      submissionResults.push(submissionResult);
+//       const submissionResult = {
+//         testCaseId: testCases[i].id,
+//         output,
+//         correct,
+//       };
+//       submissionResults.push(submissionResult);
 
-      // Only show the test case if it is marked as show
-      if (testCases[i].show == true) {
-        const resultItem = {
-          stdin: testCases[i].stdin,
-          output,
-          correct,
-          show: true,
-        };
-        results.push(resultItem);
-      }
-    }
+//       // Only show the test case if it is marked as show
+//       if (testCases[i].show == true) {
+//         const resultItem = {
+//           stdin: testCases[i].stdin,
+//           output,
+//           correct,
+//           show: true,
+//         };
+//         results.push(resultItem);
+//       }
+//     }
 
-    // Save submission to database
-    const submission = await Submission.create({
-      sourceCode,
-      totalPoint: totalPoint,
-      createdBy: userId,
-      problemId,
-    });
+//     // Save submission to database
+//     const submission = await Submission.create({
+//       sourceCode,
+//       totalPoint: totalPoint,
+//       createdBy: userId,
+//       problemId,
+//     });
 
-    // Add submission id to each submission result
-    for (submissionResult of submissionResults) {
-      submissionResult.submissionId = submission.id;
-    }
+//     // Add submission id to each submission result
+//     for (submissionResult of submissionResults) {
+//       submissionResult.submissionId = submission.id;
+//     }
 
-    // Save submission results to database
-    await SubmissionResult.bulkCreate(submissionResults);
+//     // Save submission results to database
+//     await SubmissionResult.bulkCreate(submissionResults);
 
-    res.status(201).send({ data: { id: submission.id } });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ error: translate('internal_server_error', req.hl) });
-  }
-};
+//     res.status(201).send({ data: { id: submission.id } });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send({ error: translate('internal_server_error', req.hl) });
+//   }
+// };
 
 const getSubmissionDetail = async (req, res) => {
   try {
@@ -172,9 +170,125 @@ const getSubmissionDetail = async (req, res) => {
 const submissionCallback = async (req, res) => {
   try {
     console.log(req.body);
-    res.status(200).send({ data: 'Hello from webhook' });
+    return res.sendStatus(204);
   } catch (err) {
     console.log(err);
+    res.status(500).send({ error: translate('internal_server_error', req.hl) });
+  }
+};
+
+const createSubmission = async (req, res) => {
+  try {
+    // Step 1:
+    // Authenticate middleware has attached the user to the request object
+    const userId = req.user.id;
+    const sourceCode = req.body.sourceCode;
+    const problemId = req.body.problemId;
+
+    if (!sourceCode) {
+      return res
+        .status(400)
+        .send({ error: translate('required_source_code', req.hl) });
+    }
+
+    if (!problemId) {
+      return res
+        .status(400)
+        .send({ error: translate('required_problem_id', req.hl) });
+    }
+
+    // Step 2:
+    const submission = await Submission.create({
+      sourceCode,
+      totalPoint: 0,
+      createdBy: userId,
+      problemId,
+    });
+    res.status(201).send({ data: { id: submission.id } });
+
+    // Step 3:
+    const problem = await Problem.findByPk(problemId, {
+      include: {
+        model: TestCase,
+        as: 'testCases',
+        where: { active: true },
+      },
+    });
+
+    const testCases = problem.testCases;
+    const inputSubmissions = [];
+    for (testCase of testCases) {
+      const submission = {
+        source_code: sourceCode,
+        language_id: problem.languageId,
+        stdin: testCase.stdin,
+        expected_output: testCase.expectedOutput,
+        callback_url: `${process.env.DOCKER_LOCALHOST}/api/v1/submissions/callback`,
+      };
+
+      inputSubmissions.push(submission);
+    }
+
+    // Submit code to Judge0
+    submit(inputSubmissions);
+
+    // let totalPoint = 0;
+    // const submissionResults = [];
+    // const results = [];
+    // for (var i = 0; i < judge0Submissions.length; i++) {
+    //   const correct = judge0Submissions[i].status.id == 3;
+    //   if (correct) {
+    //     totalPoint += problem.pointPerTestCase;
+    //   }
+
+    //   let output =
+    //     judge0Submissions[i].stdout ||
+    //     judge0Submissions[i].stderr ||
+    //     judge0Submissions[i].compile_output ||
+    //     '';
+    //   // Replace all \n with ''
+    //   output = output.replace(/\n/g, '');
+    //   // Decode base64
+    //   output = Buffer.from(output, 'base64').toString('ascii');
+
+    //   const submissionResult = {
+    //     testCaseId: testCases[i].id,
+    //     output,
+    //     correct,
+    //   };
+    //   submissionResults.push(submissionResult);
+
+    //   // Only show the test case if it is marked as show
+    //   if (testCases[i].show == true) {
+    //     const resultItem = {
+    //       stdin: testCases[i].stdin,
+    //       output,
+    //       correct,
+    //       show: true,
+    //     };
+    //     results.push(resultItem);
+    //   }
+    // }
+
+    // // // Save submission to database
+    // // const submission = await Submission.create({
+    // //   sourceCode,
+    // //   totalPoint: totalPoint,
+    // //   createdBy: userId,
+    // //   problemId,
+    // // });
+
+    // // Add submission id to each submission result
+    // for (submissionResult of submissionResults) {
+    //   submissionResult.submissionId = submission.id;
+    // }
+
+    // // Save submission results to database
+    // await SubmissionResult.bulkCreate(submissionResults);
+
+    // res.status(201).send({ data: { id: submission.id } });
+  } catch (error) {
+    console.log(error);
     res.status(500).send({ error: translate('internal_server_error', req.hl) });
   }
 };
