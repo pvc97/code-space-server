cc43884be83f08c2229f0cd2dad200999df51cf3
pvc2209 <cuongpv2209@gmail.com>
2023-04-05 23:16:12 +0700

    Update fcm token model.

--- migrations/202302061243_create_fcm_token.js
+++ migrations/202302061243_create_fcm_token.js
@@ -25,14 +25,6 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
     });
   },
   async down(queryInterface, Sequelize) {
--- models/fcm_token.js
+++ models/fcm_token.js
@@ -26,6 +26,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'FCMToken',
+      timestamps: false,
     }
   );
   return FCMToken;
